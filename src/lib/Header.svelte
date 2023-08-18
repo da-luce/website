505cf11ade5e7b0540c2c9216c75cf72f7e6dff1
Dalton Luce <daltonluce42@gmail.com>
2023-08-17 23:02:06 -0400

    begin porting old website to svelte

---
+++ src/lib/Header.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+    import AnimatedIcon from './components/header/AnimatedIcon.svelte'
+    import svelteLogo from '../assets/svelte.svg'
+    import Icon from './Icon.svelte'
+
+    let nav_items = [
+        { name: 'home', href: '#home', color: '#e27878', id: 1 },
+        { name: 'experience', href: '#experience', color: '#b4be82', id: 2 },
+        { name: 'contact', href: '#contact', color:'#b4be82', id: 3 },
+        { name: 'about', href: '#about', color: '#84a0c6', id: 4 },
+    ];
+    let active_nav_id = -1;
+
+    let social_icons = [
+
+    ];
+</script>
+
+<header class="site-header">
+    <div class="wrapper site-header__wrapper">
+        <div class="logo-and-name">
+            <img src={svelteLogo} class="logo" alt="Vite Logo" />
+            <h3>Dalton Luce</h3>
+        </div>
+        <nav class="nav-items">
+            {#each nav_items as item, index (item.id)}
+                <div class="nav-item">
+                    <p style:opacity={active_nav_id == item.id ? 1 : 0} style:color={item.color}>●</p>
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={item.href}
+                        style="color: {item.color};"
+                        on:mouseenter={() => {
+                            active_nav_id = item.id
+                        }}
+                        on:mouseleave={() => {
+                            active_nav_id = -1
+                        }}
+                    >
+                        {item.name}
+                    </a>
+                </div>
+            {/each}
+        </nav>
+        <div class="social-icons">
+            <Icon name="trash" />
+        </div>
+    </div>
+</header>
+
+<style>
+    .site-header {
+        top: 0;
+        width: 100%;
+        background-color: var(--background-primary);
+    }
+
+    .site-header__wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    }
+
+    .site-header__wrapper > div {
+        /* Have items take up equal space */
+        flex: 1 1 0px;
+    }
+
+    .logo-and-name > * {
+        display: inline-block;
+    }
+
+    .nav-items {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        gap: 2rem;
+    }
+
+    .nav-item {
+        display: flex;
+        align-items: center;
+    }
+
+    .nav-item > a {
+        text-decoration: none;
+    }
+
+    .nav-item > p {
+        transition: 0.2s;
+        padding: 0rem 0.25rem 0rem 0rem;
+        font-weight: bolder;
+    }
+</style>
---
+++ src/lib/components/header/AnimatedIcon.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    let svg_path:string;
+    let link:string;
+    let title:string;
+</script>
+
+<a href={link} target="_blank">
+    <svg class="icon" 
+        role="graphics-symbol"
+        xmlns="http://www.w3.org/2000/svg">
+        <title>{title}</title>
+        <path d={svg_path} pathLength="1"/>
+    </svg>
+</a>
+
+
+<style>
+    /* Doesn't work in chrome! */
+    .icon {
+        height: 2.5rem;
+        stroke-linecap: round;
+        animation-name: draw;
+        animation-duration: 3s;
+        animation-timing-function: ease;
+        animation-iteration-count: infinite;
+        animation-play-state: paused;
+        animation-fill-mode: initial;
+    }
+
+    .icon:hover {
+        animation-play-state: running;
+    }
+
+    .icon path {
+        stroke-width: 0px; /* no stroke width to avoid writing over fill in regular state */
+        fill: var(--foreground);
+        stroke: var(--background-primary);
+        transition: all 0.5s ease;
+    }
+
+    .icon:hover path {
+        stroke-width: 1px;
+        fill: var(--background-primary);
+        stroke: var(--foreground);
+    }
+
+    @keyframes draw {
+        from {
+            stroke-dashoffset: 1.25;
+            stroke-dasharray: 1 0.25;
+        }
+
+        to {
+            stroke-dashoffset: 0;
+            stroke-dasharray: 1 0;
+        }
+    }
+</style>
