d4f209fa840da871b9c919089f79bef3d806211c
Dalton Luce <daltonluce42@gmail.com>
2023-11-25 16:34:42 -0500

    Add name and background

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,9 @@
   import Landing from './lib/Landing.svelte'
 </script>
 
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet">
+</head>
 <Landing></Landing>
