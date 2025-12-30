91ac275188c633b951200d354a74c5bbb0f53e82
Dalton Luce <daltonluce42@gmail.com>
2025-12-29 21:56:32 -0500

    feat: update cdn url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,2 @@
 // S3 blob URL - all assets served directly from S3
-export const BLOB = 'https://d3h89501o7dtvo.cloudfront.net'
-// TODO: make this a nicer url
+export const BLOB = 'https://cdn.daltonluce.com'
